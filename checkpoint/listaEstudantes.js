91342a5f1660248ae4370c26589e2e6f0a2606ed
Rodrigo Rodrigues Leal da Silva <63212120+RR-LEAL@users.noreply.github.com>
2021-12-08 21:01:14 -0300

    prova final

---
+++ checkpoint/listaEstudantes.js
@@ -0,0 +1,11 @@
+listaEstudantes = [
+  { nome: "rodrigo rodrigues", qtdFaltas: 1, notas: [10, 10, 10] },
+  { nome: "marcus rodrigues", qtdFaltas: 3, notas: [10, 8, 9] },
+  { nome: "bruno neves", qtdFaltas: 2, notas: [8.7, 7, 8.8] },
+  { nome: "mateus praxedes", qtdFaltas: 3, notas: [8.7, 6, 9] },
+  { nome: "moises bollela", qtdFaltas: 2, notas: [7, 8.8, 5] },
+  
+];
+
+
+module.exports = JSON.stringify(listaEstudantes);
